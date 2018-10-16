c2f7af935774c6ea6833db5d2c38120111a8d582
sureshezhil <sureshiknow@gmail.com>
2018-10-16 15:38:34 +0530

    test

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
-    with open('user_images.csv', 'r') as readFile:
-        reader = csv.reader(readFile)
-        lines = list(reader)
-        for x in lines:
-            if(x[0]==s):
-                x[0] = user_name
+import csv
 
-    with open('user_images.csv', 'w') as writeFile:
-        writer = csv.writer(writeFile)
-        writer.writerows(lines)
+with open('user_images.csv', 'r') as readFile:
+	reader = csv.reader(readFile)
+	lines = list(reader)
+	print(list(reader))
+	print(lines)
 
-    readFile.close()
-    writeFile.close()
+    # with open('user_images.csv', 'w') as writeFile:
+    #     writer = csv.writer(writeFile)
+    #     writer.writerows(lines)
+
+readFile.close()
+    # writeFile.close()
