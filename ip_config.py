a8379ed83fa98f6bbc6e4c60e40e7b972f0d3e2d
sureshezhil <sureshiknow@gmail.com>
2018-10-17 14:13:15 +0530

    17102018_02:14

--- ip_config.py
+++ ip_config.py
@@ -1,7 +1,10 @@
 
 import sys
-
-arguments = sys.argv[2]
-arg=arguments.split(':')
-local_ip=arg[0]
-port=arg[1]
+if(len(sys.argv)>=3):
+	arguments = sys.argv[2]
+	arg=arguments.split(':')
+	local_ip=arg[0]
+	port=arg[1]
+else:
+	local_ip='127.0.0.1'
+	port='8000'
---
+++ vj.py
@@ -0,0 +1,18 @@
+# # da='2,3,'
+# # for i in da:
+# # 	print (i)
+
+# for n in range(20):
+# 	for i in range(n):
+# 		print (n, end=" ")
+# 	print("\n")	
+
+print("print data like half primade")
+r=input("enter no:")
+ra=int(r)
+data.ta
+data['ta']
+for i in range (ra,0,-1):
+	for j in range(0,i,+1):
+		print("*",end=" ")
+	print("\r")	
